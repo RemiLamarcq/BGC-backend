9077f936c8c452c53d804a7b0aac5aac12a956f4
Rémi <remi.lamarcq@yahoo.fr>
2024-03-04 15:07:17 +0100

    commit RL back au 4/03

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkbody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['email', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Check if the user has not already been registered
+  User.findOne({ email: req.body.email }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        email: req.body.email,
+        password: hash,
+        username: req.body.username,
+        token: uid2(32),
+        friendsName : req.body.username
+      });
+
+      newUser.save().then(newUser => {
+        res.json({ result: true, token: newUser.token });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
 });
+
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['email', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ email: req.body.email }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token, username: data.username});
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
+});
+
+
 
 module.exports = router;
